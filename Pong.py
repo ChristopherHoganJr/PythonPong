5a95b6cde9db77913d12667e56de6014f27588be
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-08-23 10:32:41 -0700

    changed paddle size

--- Pong.py
+++ Pong.py
@@ -22,7 +22,7 @@ ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
 ball_vel_x, ball_vel_y = 0.2, 0.2
 
 # Paddle Dimensions
-paddle_width, paddle_height = 20, 120
+paddle_width, paddle_height = 15, 130
 left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH -(100 - paddle_width/2)
 right_paddle_vel = left_paddle_vel = 0
